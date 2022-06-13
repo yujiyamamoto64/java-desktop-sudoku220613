8622ab2038ba84fe7ad090edd0df8f5f34b7d59c
yujiyamamoto64 <yujiyamamoto64@gmail.com>
2022-06-13 09:49:15 -0300

    Sudoku game class

---
+++ src/sudoku/programdomain/SudokuGame.java
@@ -0,0 +1,27 @@
+package sudoku.programdomain;
+
+import java.io.Serializable;
+
+public class SudokuGame implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private final GameState gameState;
+	private final int[][] gridState;
+	
+	public static final int GRID_BOUNDARY = 9;
+	
+	public SudokuGame(GameState gameState, int[][] gridState) {
+		super();
+		this.gameState = gameState;
+		this.gridState = gridState;
+	}
+
+	public GameState getGameState() {
+		return gameState;
+	}
+
+	public int[][] getCopyOfGridState() {
+		return SudokuUtilities.copyToNewArray(gridState);
+	}
+	
+}
